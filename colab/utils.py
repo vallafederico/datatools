d7d77eae7732584b8d19674a2d452b17048b22f3
Federico Valla <38297526+vallafederico@users.noreply.github.com>
2022-02-13 21:51:16 +0100

    Update utils.py

--- colab/utils.py
+++ colab/utils.py
@@ -1,4 +1,17 @@
-# Check GPU (or factory Reset)
+# --------- --------- Check GPU
 
 # GPU -- V100 -- Excellent / P100 -- Very Good / T4 -- Good / K80 -- Meh / P4 -- (Not Recommended)
 !nvidia-smi
+
+# --------- --------- Download Assets
+
+# @title **Download Stuffs**
+from google.colab import files
+
+max = 8#@param {type:"number"}
+prefix = "out_" #@param {type:"string"}
+
+for i in range(max):
+  filename = f"{prefix}{i}.png"
+  files.download(filename)
+
